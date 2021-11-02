e6bb4691dda83fb20a6d6446107cfd3368a2dd67
Bill Kolicoski <bkolicoski@gmail.com>
2021-11-02 21:18:54 +0100

    Updated code so it runs on ESP32 instead

--- arduino-pellet-burner-esp32.ino
+++ arduino-pellet-burner-esp32.ino
@@ -5,26 +5,84 @@
 //#define ENCODER_OPTIMIZE_INTERRUPTS
 #include <Encoder.h>
 
-ThreeWire myWire(8, 7, 9); // DAT, CLK, RST
+ThreeWire myWire(14, 12, 27); // DAT, CLK, RST
 RtcDS1302<ThreeWire> rtc(myWire);
 LiquidCrystal_I2C lcd(0x27, 20, 4);
-Encoder myEnc(3, 4);
+Encoder myEnc(5, 18);
+
+#define SWITCH_PIN 19
+#define FEED_PIN 23
+#define PUMP_PIN 25
+#define HEATER_PIN 26
+
+#define TRM_WATER 32
+
+// NTC Settings
+#define THERMISTORNOMINAL 10000 // resistance at 25 degrees C
+#define TEMPERATURENOMINAL 25   // temp. for nominal resistance (almost always 25 C)
+#define NUMSAMPLES 5            // how many samples to take and average, more takes longer but is more 'smooth'
+#define BCOEFFICIENT 3950       // The beta coefficient of the thermistor (usually 3000-4000)
+#define SERIESRESISTOR 10000   // the value of the 'other' resistor 
+
+// settings addresses
+#define TARGET_T           0
+#define IGNITION_TM        1
+#define STABILIZATION_TM   2
+#define CLEAN_INT          3
+#define CLEAN_DUR          4
+#define START_DOSE         5
+#define P1_DOSE            6
+#define P2_DOSE            7
+#define P3_DOSE            8
+#define P1_FAN             9
+#define P2_FAN            10
+#define P3_FAN            11
+#define STABILIZIE_DOSE   12
+#define STABILIZIE_FAN    13
+#define CUT_OFF_T         14
+#define SMOKE_MIN_T       15
+#define SMOKE_MAX_T       16
+#define PUMP_T            17
+#define TIME_YEAR         18
+#define TIME_MONTH        19
+#define TIME_DAY          20
+#define TIME_HOUR         21
+#define TIME_MINUTE       22
+
+int tmp_water = 20;
+int tmp_water_old = 0;
 String timeStr = "";
 String status = "stand-by";
 long oldPosition  = 0;
 long initPosition = -999;
 unsigned long menuTriggeredTime = 0;
 RtcDateTime now, ignitionStartTime, shutdownStartTime;
-const int numOfScreens = 7;
+const int numOfScreens = 23;
 int currentScreen = -1;
 String screens[numOfScreens][3] = {
-  {"Operating Tmp", "Celsius", "50"},
-  {"Ignition Time", "Minutes", "18"},
-  {"Stabilization", "Minutes", "6"},
-  {"Cleaning interval", "Minutes", "30"},
-  {"Cleaning duration", "Seconds", "15"},
-  {"Start dose", "Seconds", "10"},
-  {"Cut-off Tmp", "Celsius", "80"},
+  {"Target tmp.", "Celsius", "50"}, //TARGET_T
+  {"Ignition time", "Minutes", "18"}, //IGNITION_TM
+  {"Stabilization", "Minutes", "6"}, //STABILIZATION_TM
+  {"Cleaning interval", "Minutes", "30"}, //CLEAN_INT
+  {"Cleaning duration", "Seconds", "15"}, //CLEAN_DUR
+  {"Start dose", "Seconds", "10"}, //START_DOSE
+  {"P1 dose", "%", "10"}, //P1_DOSE
+  {"P2 dose", "%", "25"}, //P2_DOSE
+  {"P3 dose", "%", "30"}, //P3_DOSE
+  {"P1 fan", "%", "10"}, //P1_FAN
+  {"P2 fan", "%", "25"}, //P2_FAN
+  {"P3 fan", "%", "30"}, //P3_FAN
+  {"Stabilize dose", "%", "10"}, //STABILIZIE_DOSE
+  {"Stabilize fan", "%", "10"}, //STABILIZIE_FAN
+  {"Cut-off tmp.", "Celsius", "80"}, //CUT_OFF_T
+  {"Smoke tmp. min.", "Celsius", "42"}, //SMOKE_MIN_T
+  {"Smoke tmp. max.", "Celsius", "150"}, //SMOKE_MAX_T
+  {"Pump tmp.", "Celsius", "30"}, //PUMP_T
+  {"Year", "", "2021"}, //TIME_YEAR
+  {"Month", "", "1"}, //TIME_MONTH
+  {"Day", "", "1"}, //TIME_DAY
+  {"Hour", "", "0"}, //TIME_HOUR
+  {"Minute", "", "0"}, //TIME_MINUTE
 };
 int parameters[numOfScreens];
 bool updateScreen = true;
@@ -37,24 +95,26 @@ void setup() {
   //TODO: Remove serial when no longer needed
   Serial.begin(115200);
   Serial.println(ignitionStartTime);
-  pinMode(2, INPUT_PULLUP);
-  pinMode(10, OUTPUT); //feed
-  digitalWrite(10, HIGH);
-  pinMode(11, OUTPUT); //pump
-  digitalWrite(11, HIGH);
-  pinMode(12, OUTPUT); //heater
-  digitalWrite(12, HIGH);
-  attachInterrupt(digitalPinToInterrupt(2), handleButtonChange, CHANGE);
+  pinMode(SWITCH_PIN, INPUT_PULLUP);
+  pinMode(FEED_PIN, OUTPUT); //feed
+  digitalWrite(FEED_PIN, HIGH);
+  pinMode(PUMP_PIN, OUTPUT); //pump
+  digitalWrite(PUMP_PIN, HIGH);
+  pinMode(HEATER_PIN, OUTPUT); //heater
+  digitalWrite(HEATER_PIN, HIGH);
+  attachInterrupt(SWITCH_PIN, handleButtonChange, CHANGE);
   initScreen();
   initTime();
   delay(2000);
-  clearLCDLine(2);
+  printOperatingTemperature();
   clearLCDLine(3);
+  updateLCDStatus();
 }
 
 void loop() {
   long newPosition = myEnc.read();
   now = rtc.GetDateTime();
+  //encoder turn handling
   if (newPosition != oldPosition && newPosition % 2 == 0) {
     if (menuTriggeredTime != 0 && currentScreen != -1) {
       if (newPosition > oldPosition) {
@@ -71,11 +131,12 @@ void loop() {
       } else if(newPosition < oldPosition){
         parameters[0]--;
       }
+      printOperatingTemperature();
     }
     oldPosition = newPosition;
-    updateScreen = true;
   }
 
+  //settings trigger handling
   if (menuTriggeredTime != 0 && currentScreen != -1) {
     displaySettingsMenu();
     if (menuTriggeredTime + 4000 < millis()) {
@@ -87,9 +148,12 @@ void loop() {
       oldPosition = initPosition;
       newPosition = initPosition;
       initPosition = -999;
-      updateScreen = true;
+      printOperatingTemperature();
+      printCurrentTemperature();
+      updateLCDStatus();
     }
   } else {
+    //no clicking, handle other logic
     updateTime(returnDateTime(now));
     if (!now.IsValid())
     {
@@ -97,16 +161,20 @@ void loop() {
       //    1) the battery on the device is low or even missing and the power line was disconnected
       Serial.println("RTC lost confidence in the DateTime!");
     }
+
+    //measure current temperature
+    tmp_water = measureTemperature(TRM_WATER);
+    if(tmp_water != tmp_water_old) {
+      printCurrentTemperature();
+      tmp_water_old = tmp_water;
+    }
   }
 
   resolveStatusState();
 
   resolveHeaterState();
-
   if(updateScreen) {
     updateLCDStatus();
-    printOperatingTemperature();
-    printCurrentTemperature();
     updateScreen = false;
   }
 
@@ -115,17 +183,17 @@ void loop() {
 
 void resolveStatusState() {
   if(status == "ignition") {
-    //TODO: Add proper settings delay check for ifnition time instead of 30
+    //TODO: Add proper settings delay check for ignition time instead of 30
     if(ignitionStartTime + 30 < now) {
       //TODO: Add exaust temperature check to confirm flame
       status = "stabilization";
-      updateScreen = true;
+      updateLCDStatus();
     }
   } else if(status == "stabilization") {
     //TODO: Add proper delay for ignition + stabilization time
     if(ignitionStartTime + 60 < now) {
       status = "heating";
-      updateScreen = true;
+      updateLCDStatus();
     }
   } else if(status == "shutdown") {
     //TODO: Add proper delay for ignition + stabilization time
@@ -135,17 +203,52 @@ void resolveStatusState() {
       ignitionStartTime = NULL;
       shutdownStartTime = NULL;
       Serial.println(ignitionStartTime);
-      updateScreen = true;
+      updateLCDStatus();
     }
   }
 }
 
 void resolveHeaterState() {
   if(status == "ignition") {
-    digitalWrite(12, LOW);
+    //activate
+    digitalWrite(HEATER_PIN, LOW);
   } else {
-    digitalWrite(12, HIGH);
+    digitalWrite(HEATER_PIN, HIGH);
   }
+}
+
+int measureTemperature(int pinNumber) {
+  uint8_t i;
+  float average;
+  int samples[NUMSAMPLES];
+ 
+  // take N samples in a row, with a slight delay
+  for (i=0; i< NUMSAMPLES; i++) {
+   samples[i] = analogRead(pinNumber);
+   delay(10);
+  }
+ 
+  // average all the samples out
+  average = 0;
+  for (i=0; i< NUMSAMPLES; i++) {
+     average += samples[i];
+  }
+  average /= NUMSAMPLES;
+
+  // convert the value to resistance
+  average = 4095 / average - 1;
+  average = SERIESRESISTOR / average;
+
+  float steinhart;
+  steinhart = average / THERMISTORNOMINAL;     // (R/Ro)
+  steinhart = log(steinhart);                  // ln(R/Ro)
+  steinhart /= BCOEFFICIENT;                   // 1/B * ln(R/Ro)
+  steinhart += 1.0 / (TEMPERATURENOMINAL + 273.15); // + (1/To)
+  steinhart = 1.0 / steinhart;                 // Invert
+  steinhart -= 273.15;                         // convert to C
+
+  return round(steinhart);
+  //TODO: Add temperature offset for adjustment
 }
 
 void initScreen() {
@@ -190,7 +293,7 @@ void initTime() {
     rtc.SetIsRunning(true);
   }
 
-  RtcDateTime now = rtc.GetDateTime();
+  now = rtc.GetDateTime();
   if (now < compiled)
   {
     Serial.println("RTC is older than compile time!  (Updating DateTime)");
@@ -216,12 +319,12 @@ void printOperatingTemperature() {
 }
 
 void printCurrentTemperature() {
-  clearLCDLine(2);
-  lcd.setCursor(0, 2);
-  lcd.print("Current tmp: ");
-  lcd.print("xx"); //TODO: Add reading from temperature probe
-  lcd.print((char)223);
-  lcd.print("C");
+    clearLCDLine(2);
+    lcd.setCursor(0, 2);
+    lcd.print("Current tmp: ");
+    lcd.print(tmp_water);
+    lcd.print((char)223);
+    lcd.print("C");
 }
 
 void updateLCDStatus() {
@@ -265,7 +368,7 @@ String returnDateTime(const RtcDateTime& dt) {
 
 void handleButtonChange()
 {
-  if(digitalRead(2) == HIGH) {
+  if(digitalRead(SWITCH_PIN) == HIGH) {
     //release
     if(menuTriggeredTime + 3000 <= millis()) {
       if(status == "stand-by") {
